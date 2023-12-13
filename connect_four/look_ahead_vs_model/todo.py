313b6a3e3cdfa7b318d867d977a571bc87de1c0c
benmoore12495ut <153758278+benmoore12495ut@users.noreply.github.com>
2023-12-13 16:38:38 -0600

    Create todo.py

---
+++ connect_four/look_ahead_vs_model/todo.py
@@ -0,0 +1,7 @@
+Overall goal: collect data that learns and updates as more games are played and then train a tensorflow model on that..
+
+Steps:
+0) Watch this video: https://www.youtube.com/watch?v=MMLtza3CZFM
+# [DONE] 1) Make a file that allows 2 look aheads to play against each other -- "BUILD MODEL TRAINING DATA.PY"
+# [DONE] 2) Log that data incrementaly into csv
+1) Get model to play as player 2 and still beat the old model 
